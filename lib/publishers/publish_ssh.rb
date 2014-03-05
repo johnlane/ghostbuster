e71aa0e188a7b7a6a363700c0b8854f7e0ff64f7
John Lane <john@lane.uk.net>
2014-03-05 18:28:18 +0000

    SSH publisher

---
+++ lib/publishers/publish_ssh.rb
@@ -0,0 +1,31 @@
+require 'net/scp' # gem install net-scp
+require 'etc'
+
+class Publisher
+
+private
+
+  # FTP upload creates a compressed (bzip2) TAR archive of the export and uploads it
+  # to the remote server. A companion script on the server should extract the archive
+  # into the relevant location. An example is given in 'extras/ghostbuster-ftp-extract'
+  def ssh(src,uri)
+    log "Publishing #{src} to #{uri}..."
+    
+    host = uri.host
+    user = uri.user ? uri.user : Etc.getlogin
+    pass = uri.password ? { password: uri.password } : {}
+    dest = uri.path
+
+    log "Connecting to #{host}"
+    begin
+      Net::SCP.start(host,user,pass) do |scp|
+        log "Uploading"
+        scp.upload!(src+'/.',dest, recursive: true)
+        log "Done"
+      end
+    rescue Net::SSH::AuthenticationFailed, Net::SCP::Error => e
+      abort ("SCP Failure: #{e}")
+    end
+  end
+
+end
