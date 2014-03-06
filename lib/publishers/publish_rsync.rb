f7029df41507411b71b4c4b2d71d30450b9922bd
John Lane <john@lane.uk.net>
2014-03-06 20:57:03 +0000

    rsync publisher

---
+++ lib/publishers/publish_rsync.rb
@@ -0,0 +1,25 @@
+require 'rsync' # gem install rsync
+require 'cgi'
+
+class Publisher
+
+private
+
+  # Rsync publisher using the RSYNC protocol (NOT rsync over ssh)
+  def rsync(src,uri)
+    log "Publishing #{src} to #{uri}..."
+    if uri.query
+      params = CGI.parse(uri.query).each_with_object({}){|(k,v),h| h[k.to_sym] = v} # symbolise
+    else
+      params = {}
+    end
+
+    rsync_args = params[:args] ? params[:args] : '-a'
+    dest = "#{uri.host}::#{uri.path.sub(/^\//,'')}"
+
+    log "Copying..."
+    Rsync.run(src+'/.',dest, rsync_args.split) { |r| abort(r.error) unless r.success? }
+    log "Done"
+  end
+
+end
