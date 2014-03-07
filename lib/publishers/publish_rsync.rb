5c1f372a0562c97a72436f6e81c4b182e7c9802a
John Lane <john@lane.uk.net>
2014-03-07 17:45:28 +0000

    rsync support local destination

--- lib/publishers/publish_rsync.rb
+++ lib/publishers/publish_rsync.rb
@@ -15,7 +15,11 @@ private
     end
 
     rsync_args = params[:args] ? params[:args] : '-a'
-    dest = "#{uri.host}::#{uri.path.sub(/^\//,'')}" # destination without leading /
+    if uri.host
+      dest = "#{uri.host}::#{uri.path.sub(/^\//,'')}" # destination without leading /
+    else
+      dest = uri.path
+    end
 
     log "Copying..."
     Rsync.run(src+'/.',dest, rsync_args.split) { |r| abort(r.error) unless r.success? }
