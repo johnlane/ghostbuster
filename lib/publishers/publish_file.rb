7627455c4298c4369b00bbf561d819334d5f01e2
John Lane <john@lane.uk.net>
2014-03-07 12:53:10 +0000

    File publisher preserve timestamps

--- lib/publishers/publish_file.rb
+++ lib/publishers/publish_file.rb
@@ -7,7 +7,7 @@ private
   # File publisher copies the temporary export space to a local path
   def file(src,uri)
     log "Publishing #{src} to #{uri}..."
-    FileUtils.cp_r(src+'/.',uri.path)
+    FileUtils.cp_r(src+'/.',uri.path, preserve: true)
     log "Done"
   end
 
