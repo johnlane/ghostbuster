ca3c43ee1d369fbe02a237486f791a32b63120e5
John Lane <john@lane.uk.net>
2014-05-11 16:50:38 +0100

    handle jekyll frontmatter

--- lib/export_filters/export_jekyll.rb
+++ lib/export_filters/export_jekyll.rb
@@ -3,14 +3,33 @@ require 'fileutils'
 
 # Add supporting methods to the Post extension
 module Post
-  def front_matter # http://jekyllrb.com/docs/frontmatter
-    fm = "---"+NL
-    fm << "layout: post"+NL
-    fm << "title: \"#{title}\""+NL
-    fm << "date: #{date format: 'YYYY-MM-DD HH:mm:ss'}"+NL
-    fm << "categories: #{tags.join(' ')}"+NL
-    fm << "permalink: #{slug}.html"+NL
-    fm << "---"+NL
+
+  # Return the posts's markup with Jekyll front-matter applied:
+  # a combination of default front-matter defined below that is
+  # augmented with any supplied in the post.
+  def markdown
+    md = super
+    frontmatter= { 'layout'     => 'post',
+                   'title'      => title,
+                   'date'       => date(format: 'YYYY-MM-DD HH:mm:ss'),
+                   'categories' => tags.join(' '),
+                   'permalink'  => slug+'.html'
+                  }
+    match_regex = /^---$(.*?)^---$/m
+    
+    # augment frontmatter with any already in post
+    if fm_match = md.match(match_regex)
+      fm_match[1].strip.split("\n").each do |s|
+        k,v = s.split(':').each{|s| s.strip!}
+        frontmatter[k] = v
+      end
+    end
+
+    # write frontmatter into post
+    fm_str = %{---\n#{frontmatter.map{|k,v| "#{k}: #{v}"}.join("\n")}\n---\n}
+    md.insert(0,fm_str) unless md.sub!(match_regex,fm_str)
+
+    md
   end
 end
 
@@ -30,7 +49,7 @@ class ExportFilter_jekyll < ExportFilter
     extend_post(post,'md')
 
     # get markdown content
-    content = post.front_matter + post.markdown
+    content = post.markdown
 
     # custom layout
     content.sub!(/^(layout: ).*/,'\1'+params[:page_layout]) if post.page? and params[:page_layout]
